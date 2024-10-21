f2eac078425853ea1b03d19b547db3df707291fc
Yashvardhan Jaiswal <136220426+Yashvardhan0718@users.noreply.github.com>
2024-10-21 19:57:52 +0530

    Update mongoose.js

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 const mongoURI = process.env.MONGODB_URI || 'mongodb://0.0.0.0:27017/RoomerAPI'
 
-mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true});
+mongoose.connect(mongoURI, { keepAlive: true, useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true});
 
 module.exports = { mongoose }  // Export the active connection.
